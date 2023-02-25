6180ab26541b515386551c2a20e849d4704c6c91
mustafa <mustafa.bhm3@gmail.com>
2023-02-25 13:40:06 -0700

    change the path for the build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Form from "./components/Form";
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
     <div className="App">
+      <HomePage />
       <Form />
     </div>
   );
--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,6 +1,12 @@
 import React from "react";
 
 function HomePage() {
+  const location = {
+    address: "1600 Amphitheatre Parkway, Mountain View, california.",
+    lat: 37.42216,
+    lng: -122.08427,
+  };
+
   return <div>HomePage</div>;
 }
 
