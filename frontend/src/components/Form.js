480328a171433ed7089bfb4cd4879738f2b09bb8
mustafa <mustafa.bhm3@gmail.com>
2023-02-25 11:52:39 -0700

    adding react + form for file upload

---
+++ frontend/src/components/Form.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+function Form() {
+  const [selectedFile, setSelectedFile] = useState();
+  const [isSelected, setIsSelected] = useState(false);
+
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+    setIsSelected(true);
+  };
+
+  const handleSubmission = () => {
+    const formData = new FormData();
+
+    formData.append("File", selectedFile);
+
+    fetch("https://freeimage.host/api/1/upload?key=<YOUR_API_KEY>", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log("Success:", result);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  return (
+    <div className="form">
+      <h2>Upload a File</h2>
+
+      {isSelected ? (
+        <div>
+          <p>Filename: {selectedFile.name}</p>
+          <p>Filetype: {selectedFile.type}</p>
+          <p>Size in bytes: {selectedFile.size}</p>
+        </div>
+      ) : (
+        <p>Select a file to upload</p>
+      )}
+      <div className="form-bottom">
+        <input type="file" name="file" onChange={changeHandler} />
+        <button className="btn" onClick={handleSubmission}>
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Form;
