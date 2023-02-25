1ec512aca09792990b21cb7dd73126cb6ad99db7
elhardoum <elhardoum@outlook.com>
2023-02-25 15:30:22 -0700

    salvage a demo

--- backend/config.py
+++ backend/config.py
@@ -4,3 +4,4 @@ FLASK_SECRET=b'EHoPoGX2DLKIqEhMxZlpFkritOyZTeMVpp'
 MODEL_LOCATION=path.join(path.dirname(path.realpath(__file__)), './model_1.h5')
 MODEL_SETTINGS_LOCATION=path.join(path.dirname(path.realpath(__file__)), './model_1.json')
 REACT_BUILD_DIR=path.join(path.dirname(path.realpath(__file__)), './../build')
+IMAGES_MAP_CSV_FILE=path.join(path.dirname(path.realpath(__file__)), './permian_source_list_2019.csv')
--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -4,6 +4,9 @@ import React, { useState } from "react";
 function Form() {
   const [selectedFile, setSelectedFile] = useState();
   const [isSelected, setIsSelected] = useState(false);
+  const [_lat, setLat] = useState();
+  const [_lng, setLng] = useState();
+  const [pred, setPred] = useState();
 
   const changeHandler = (event) => {
     setSelectedFile(event.target.files[0]);
@@ -15,13 +18,25 @@ function Form() {
 
     formData.append("File", selectedFile);
 
+    setLat(undefined)
+    setLng(undefined)
+    setPred(undefined)
+
     fetch("/api/post-image", {
       method: "POST",
       body: formData,
     })
       .then((response) => response.json())
       .then((result) => {
-        console.log("Success:", result);
+        const {lat, lng, dataset} = result
+        if ( lat && lng ) {
+          setLat(lat)
+          setLng(lng)
+        }
+
+        if ( dataset && dataset[0] ) {
+          setPred(dataset[0])
+        }
       })
       .catch((error) => {
         console.error("Error:", error);
@@ -47,6 +62,9 @@ function Form() {
           Submit
         </button>
       </div>
+        { !!(pred) && <pre>Prediction: {pred}</pre> }
+
+          { Boolean(_lat && _lng) && <img src={`https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyAlj0pxuucJSlRM71sIzoHXVQMqXk9C1Yo&markers=${_lat},${_lng}&size=640x640&zoom=11`} /> }
     </div>
   );
 }
