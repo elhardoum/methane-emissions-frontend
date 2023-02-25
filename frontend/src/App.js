139c3f5aeffd10e246de458bf909c69b6b97e228
mustafa <mustafa.bhm3@gmail.com>
2023-02-25 14:26:27 -0700

    working on google maps

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
 import Form from "./components/Form";
-import HomePage from "./components/HomePage";
+// import HomePage from "./components/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      {/* <HomePage /> */}
       <Form />
     </div>
   );
--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,13 +1,27 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+import Location from "./Location";
 
 function HomePage() {
-  const location = {
-    address: "1600 Amphitheatre Parkway, Mountain View, california.",
-    lat: 37.42216,
-    lng: -122.08427,
+  const defaultProps = {
+    center: {
+      lat: 10.99835602,
+      lng: 77.01502627,
+    },
+    zoom: 11,
   };
 
-  return <div>HomePage</div>;
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <Location lat={59.955413} lng={30.337844} text="My Marker" />
+      </GoogleMapReact>
+    </div>
+  );
 }
 
 export default HomePage;
